e103f0b9a7a031d6aca5080562d70fb5192b9a35
Albar00n <mgrushi73@gmail.com>
2023-07-14 21:38:44 +0400

    a

--- flash_script/sketch_flash.js
+++ flash_script/sketch_flash.js
@@ -11,7 +11,7 @@ var budgeCenter = 0;
 var mainFlash;
 var sceneLength = 30;
 
-var starterText = "THE\nPLUSH\nSLIK";
+var starterText = "THE\nQURASHI\nSTORE";
 // var starterText = "الحركة\nبركة";
 
 var rampCounter = 0;
